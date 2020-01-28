92e9ad2e8050beb040ffe4108188d92e088dd46b
Mihael <golob.mihael@gmail.com>
2020-01-28 19:10:08 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace IntroductionToAlgorithms {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
